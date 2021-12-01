8c2dd94823612beadc1f262e8963e1df5e3cb0e5
Mateusz <matzielinski7@gmail.com>
2021-12-01 12:01:26 +0100

    przyklejenie do góry

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-window.onscroll = function () {topBar ()};
+window.onscroll = function () {checkscroll ()};
 const navBar = document.querySelector('.navBar');
 
+function checkscroll () {
+   topBar();
+   attachRestSearch();
+}
 function topBar () {
    if (document.body.scrollTop > 0 || document.documentElement.scrollTop >0) {
       navBar.classList.add("scrolledFromTop");
    } else if (navBar.scrollTop == 0) {
       navBar.classList.remove("scrolledFromTop")
    }
-   console.log(document.body.scrollTop);
+}
+const inputRestaurant = document.querySelector(".wrapperInputRestaurant"); 
+const inpRest = document.querySelector(".inputRestaurant"); 
+function attachRestSearch () {
+   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+      inputRestaurant.classList.add("inputResaurantScrollled");
+      inpRest.classList.add("grey");
+   } else if (inputRestaurant.scrollTop <=500) {
+      inputRestaurant.classList.remove("inputResaurantScrollled")
+      inpRest.classList.remove("grey")
+   }
+   console.log(inputRestaurant.scrollTop);
 }
