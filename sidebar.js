0629b1d4e2bce9d2dd980ba67837ae11e82dd8f6
Mateusz <matzielinski7@gmail.com>
2021-12-02 11:36:08 +0100

    blokada scrolla po wysunięciu sidebara

--- sidebar.js
+++ sidebar.js
@@ -1,12 +1,16 @@
 const sidebar = document.querySelector(".sidebar");
 const navBtn = document.querySelector(".navBtn");
 const wrapper = document.querySelector(".wrapper");
+const dimmer = document.querySelector(".dimmer");
+const body = document.querySelector("body");
 navBtn.addEventListener('click', mobileMenu);
 
 
 function mobileMenu () {
    sidebar.classList.add('active');
    wrapper.classList.add('active');
+   body.classList.add('active');
+   dimmer.show();
 }
 document.addEventListener('click', function(event) {
    const isClickInsideElement = navBtn.contains(event.target);
@@ -14,5 +18,7 @@ document.addEventListener('click', function(event) {
    if ((!isClickInsideElement) && (!isClick2)) {
       sidebar.classList.remove('active');
       wrapper.classList.remove('active');
+      body.classList.remove('active');
+      dimmer.hide();
    }
 });
