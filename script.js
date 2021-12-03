d687c9a043e0e6d04b7348515b5754e6abbf0cb3
Mateusz Zieliński <matzielinski7@gmail.com>
2021-12-03 14:38:49 +0100

    pasek szukania restuaracji fajnie zrobiony

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onscroll = function () {checkscroll ()};
 const navBar = document.querySelector('.navBar');
 
+
 function checkscroll () {
    topBar();
    attachRestSearch();
@@ -8,6 +9,9 @@ function checkscroll () {
 function topBar () {
    if ((document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) && document.body.clientWidth >= 1024) {
       navBar.classList.add("scrolledFromTop");
+      const left =getPos(navBar)+300
+      inputRestaurant.style.left = `${left}px`;
+      
    } else if (navBar.scrollTop == 0) {
       navBar.classList.remove("scrolledFromTop")
    }
@@ -19,11 +23,22 @@ const inpRest = document.querySelector(".inputRestaurant");
 //przy wężyszych rozdzielczościach przeskok jest za wcześnie
 //ale w czystym js ciężko to chyba inaczej zrobić z tego co znalazłem
 function attachRestSearch () {
-   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+   const inputPosition = inputRestaurant.getBoundingClientRect();
+   const dostarczTerazBtn = document.querySelector('.dostarczTeraz');
+   const dostarczTerazBtnPosition = dostarczTerazBtn.getBoundingClientRect();
+   console.log(dostarczTerazBtnPosition.top);
+   if (inputPosition.top <= 26 && dostarczTerazBtnPosition.top < 26) {
       inputRestaurant.classList.add("inputResaurantScrollled");
       inpRest.classList.add("grey");
-   } else if (inputRestaurant.scrollTop <=500) {
-      inputRestaurant.classList.remove("inputResaurantScrollled")
-      inpRest.classList.remove("grey")
+   } else {
+      inputRestaurant.classList.remove("inputResaurantScrollled");
+      inpRest.classList.remove("grey");
    }
+}
+function getPos(el) {
+   // yay readability
+   for (var lx=0, ly=0;
+        el != null;
+        lx += el.offsetLeft, ly += el.offsetTop, el = el.offsetParent);  
+   return lx;
 }
