88c205828ea2b2a06467211f031d4227d34964dc
Mateusz <matzielinski7@gmail.com>
2021-12-06 11:46:43 +0100

    dodatkowe zdjecia pojawiajace sie w wersji mobilnej

--- mainBackground.js
+++ mainBackground.js
@@ -13,6 +13,7 @@
 function setBackground () {
    const mainImg1 = document.querySelector(".mainimg1");
    const mainImg2 = document.querySelector(".mainimg2");
+   const mainImgMobile = document.querySelector(".mainimgMobile")
    const mainImgCont = document.querySelector(".mainImgCont");
    const number = getRandomInt(1, 4);
    console.log(number);
@@ -21,23 +22,27 @@ function setBackground () {
       case 1:
          mainImg1.src= "img\\mainImgs\\mainimgset1_1.svg";
          mainImg2.src= "img\\mainImgs\\mainimgset1_2.svg";
+         mainImgMobile.src= "img\\mainImgs\\mainimgset1_mobile.svg";
          mainImgCont.style.backgroundColor = "rgb(255, 215, 210)";
          break;
       case 2: {
          mainImg1.src= "img\\mainImgs\\mainimgset2_1.svg";
          mainImg2.src= "img\\mainImgs\\mainimgset2_2.svg";
+         mainImgMobile.src= "img\\mainImgs\\mainimgset2_mobile.svg";
          mainImgCont.style.backgroundColor = "rgb(255, 192, 67)";
          break;
       }
       case 3: {
          mainImg1.src= "img\\mainImgs\\mainimgset3_1.svg";
          mainImg2.src= "img\\mainImgs\\mainimgset3_2.svg";
+         mainImgMobile.src= "img\\mainImgs\\mainimgset3_mobile.svg";
          mainImgCont.style.backgroundColor = "rgb(250, 146, 105)";
          break;
       }
       case 4: {
          mainImg1.src= "img\\mainImgs\\mainimgset4_1.svg";
          mainImg2.src= "img\\mainImgs\\mainimgset4_2.svg";
+         mainImgMobile.src= "img\\mainImgs\\mainimgset4_mobile.svg";
          mainImgCont.style.backgroundColor = "rgb(255, 242, 217)";
          break;
       }
